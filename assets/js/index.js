c39a44dfd8b05f1396cc16d75a73ec0d2bdb94b7
Vladimir <nightsense@mail.ru>
2024-01-19 19:31:00 +0300

    sdfdsf

---
+++ assets/js/index.js
@@ -0,0 +1,46 @@
+
+
+
+
+
+$("#update_user").submit(function(event){
+    event.preventDefault();
+
+    var unindexed_array = $(this).serializeArray();
+    var data = {}
+
+    $.map(unindexed_array, function(n, i){
+        data[n['name']] = n['value']
+    })
+
+
+    var request = {
+        "url" : `http://localhost:3000/api/users/${data.id}`,
+        "method" : "PUT",
+        "data" : data
+    }
+
+    $.ajax(request).done(function(response){
+
+    })
+
+})
+
+if(window.location.pathname != "/"){
+    $ondelete = $(".table2 tbody td a.delete");
+    $ondelete.click(function(){
+        var id = $(this).attr("data-id")
+
+        var request = {
+            "url" : `http://localhost:3000/api/users/${id}`,
+            "method" : "DELETE"
+        }
+
+        if(2>1){
+            $.ajax(request).done(function(response){
+                location.reload();
+            })
+        }
+
+    })
+}
